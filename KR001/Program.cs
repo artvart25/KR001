550be4a165d0bc8a8405777d43423e3eb042cd45
artvart25 <carlito77@bk.ru>
2022-08-05 13:30:15 +0300

    Added new console

---
+++ KR001/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string[] arrayOne = new string[7] {"123" , "Sochi" , "VAU" , "world" , "046" , ":-)" , "Russia"};
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+    if(arrayOne[i].Length <= 3)
+        {
+        arrayTwo[count] = arrayOne[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
